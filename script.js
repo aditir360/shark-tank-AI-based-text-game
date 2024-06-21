c595ce4fab8ebd245dc64cbd41651b48ad92dd88
aditir360 <71522856+aditir360@users.noreply.github.com>
2024-06-21 12:09:50 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+var myGame = new WizardOrpheus('', `
+You are a judge at a shark tank competition. A candidate has designed a healthcare app where an AI model detects skin disorders using computer vision. The candidate's task is to convince you to invest 20K in their product. Try to be hesistant and don't be convinced too easily! Think wisely.
+`)
+
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
+
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    myGame.message(userInput)
+
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
+
+    document.getElementById('input').value = ''
+  }
+})
+
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
+
+
+myGame.variable('score', 'Current score. Changes (positive and negatively) as the user does things.', 0)
+
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+
+document.getElementById('score').innerHTML = data.currentVariables.score.value
+})
